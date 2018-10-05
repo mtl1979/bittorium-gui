0f4a93cab60723f2031d92ca53b09768426d3791
BITTORIUM <42850689+bittorium@users.noreply.github.com>
2018-10-05 15:19:55 +0100

    Update Settings.cpp
    new pool list

--- src/Settings/Settings.cpp
+++ src/Settings/Settings.cpp
@@ -84,7 +84,8 @@ Settings& Settings::instance() {
 
 
 Settings::Settings() : m_p2pBindPort(0), m_cmdLineParser(nullptr) {
-  m_defaultPoolList << "btor.youpool.io:3339";
+  m_defaultPoolList << "pool.bittorium.org:3333";
+  m_defaultPoolList << "pool.raasu.org:3333";
   m_defaultPoolList << "btor.simpool.xyz:3223";
 
   Style* lightStyle = new LightStyle();
